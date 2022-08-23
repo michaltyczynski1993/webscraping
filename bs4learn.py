7a2eb25cbeca72d18114fc91c2869a1b316072b1
Michał Tyczyński <m.tyczynski1993@gmail.com>
2022-08-23 21:50:32 +0200

    Initial commit

---
+++ bs4learn.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get('https://realpython.github.io/fake-jobs/')
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id = 'ResultsContainer')
+job_results = results.find_all('div', class_ = 'card-content')
+
+for job_result in job_results:
+    title_element = job_result.find("h2", class_="title")
+    company_element = job_result.find("h3", class_="company")
+    location_element = job_result.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
