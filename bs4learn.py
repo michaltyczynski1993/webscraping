b3c3dc5da9aa1360aabe69fa2e087522f583c2f5
Michal Tyczynski <m.tyczynski1993@gmail.com>
2022-08-24 09:24:03 +0200

    learning bs4 selectors

--- bs4learn.py
+++ bs4learn.py
@@ -5,14 +5,17 @@ page = requests.get('https://realpython.github.io/fake-jobs/')
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id = 'ResultsContainer')
-job_results = results.find_all('div', class_ = 'card-content')
+# results = soup.find(id = 'ResultsContainer')
+# job_results = results.find_all('div', class_ = 'card-content')
 
-for job_result in job_results:
-    title_element = job_result.find("h2", class_="title")
-    company_element = job_result.find("h3", class_="company")
-    location_element = job_result.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print()
+# for job_result in job_results:
+#     title_element = job_result.find("h2", class_="title")
+#     company_element = job_result.find("h3", class_="company")
+#     location_element = job_result.find("p", class_="location")
+#     print(title_element.text.strip())
+#     print(company_element.text.strip())
+#     print(location_element.text.strip())
+#     print()
+element = soup.find('p', 'subtitle')
+
+print(element.text.strip())
